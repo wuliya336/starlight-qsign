51138f220b7b73a708b8a16589cd7f6232b064cf
wuliya <3369906077@qq.com>
2024-12-01 21:01:12 +0800

    refactor(signList): 更新 SignUtil 导入路径
    - 将 SignUtil 的导入路径从 '../model/index.js' 修改为 '../models/index.js'
    - 此修改统一了模型文件的导入路径，提高了代码的一致性和可维护性

--- apps/signList.js
+++ apps/signList.js
@@ -1,5 +1,5 @@
 import { Config, Render, Data, Version } from '../components/index.js'
-import { SignUtil } from '../model/index.js'
+import { SignUtil } from '../models/index.js'
 import { plugin, redis, Bot, segment, logger } from '../components/Base/index.js'
 
 export class sign extends plugin {
