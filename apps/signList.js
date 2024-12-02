7093017ddcfa6e3d085b365c0d7c7ba9452bffff
wuliya <3369906077@qq.com>
2024-12-02 20:22:09 +0800

    refactor(signList): 优化回复消息的代码结构
    - 将 replyMessage 数组直接传递给 e.reply 方法
    - 调整数组结构，使表情和回复设置参数直接可见
    - 移除了不必要的变量声明和额外的 await 语句

--- apps/signList.js
+++ apps/signList.js
@@ -26,11 +26,12 @@ export class sign extends plugin {
       if (!(await SignUtil.checkRedisStatus(redis, redisKey, e))) return false
     }
 
-    const replyMessage = [
+    await e.reply([
       '真是没救了,你个小杂鱼',
-      [segment.face(175)]
-    ]
-    await e.reply(replyMessage, true, { at: true })
+      segment.face(175),
+      true, 
+      { at: true }
+    ])
 
     Data.sleep(500)
     await e.reply('正在获取公共签名API列表信息，请稍候...', true, { recallMsg: 5 })
