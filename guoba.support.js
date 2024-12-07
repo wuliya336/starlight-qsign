208e1e285f105e141f955a29da21173da353a019
wuliya <3369906077@qq.com>
2024-12-08 03:28:22 +0800

    🐛 style(guoba.support.js): 将组件名 inputNumber 首字母大写
    - 将 'inputNumber' 组件名修改为 'InputNumber'，以符合组件命名的首字母大写规范

--- guoba.support.js
+++ guoba.support.js
@@ -66,7 +66,7 @@ export function supportGuoba () {
         {
           field: 'other.renderScale',
           label: '渲染精度',
-          component: 'inputNumber',
+          component: 'InputNumber',
           bottomHelpMessage:
             '可选值50~200，建议100。设置高精度会提高图片的精细度，但因图片较大可能会影响渲染与发送速度',
           required: true,
